039b343600cac176a799424cb2cba1faf0cb8a9d
fab-gentile <83715159+fab-gentile@users.noreply.github.com>
2025-06-06 15:37:59 +0000

    Deploying to gh-pages from @ fab-gentile/fab-gentile.github.io@cecd4361ec6a23b84f66dfb4f3cd205f1aceec29 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -59,12 +59,7 @@ ninja.data = [{
             window.open("https://medium.com/@al-folio/displaying-external-posts-on-your-al-folio-blog-b60a1d241a0a?source=rss-17feae71c3c4------2", "_blank");
           
         },
-      },{id: "projects-project-3-with-very-long-name",
-          title: 'project 3 with very long name',
-          description: "a project that redirects to another website",
-          section: "Projects",handler: () => {
-              window.location.href = "/projects/3_project/";
-            },},{id: "projects-project-4",
+      },{id: "projects-project-4",
           title: 'project 4',
           description: "another without an image",
           section: "Projects",handler: () => {
@@ -104,6 +99,11 @@ ninja.data = [{
           description: "a project with a background image and giscus comments",
           section: "Projects",handler: () => {
               window.location.href = "/projects/Euclid/";
+            },},{id: "projects-project-3-with-very-long-name",
+          title: 'project 3 with very long name',
+          description: "a project that redirects to another website",
+          section: "Projects",handler: () => {
+              window.location.href = "/projects/Lensing/";
             },},{
         id: 'social-email',
         title: 'email',
